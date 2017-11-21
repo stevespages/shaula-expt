1a9299ef29b12df367c0fde2de2f057a9ab2ea63
Steve Greig <greigSteve@gmail.com>
2017-11-21 22:29:03 +0000

    Added text to /index.php

--- controllers/home.php
+++ controllers/home.php
@@ -3,6 +3,8 @@
  * controllers/home.php
  */
  
- $home = "<h3>Home Page</h3><p>this will be the home page</p>";
+ $home = "<h3>Home Page</h3><p>this will be the home page</p>
+ 			<p>As of today Tu 21 Nov 2017. I am tracking this project with git. I have a mirror of the project at GitHub</p>";
+
  
  return $home;
