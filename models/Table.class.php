8a4b01f1b13f8ec8baaf5763d51c0c18bb871e20
Steve Greig <greigSteve@gmail.com>
2017-11-26 21:21:08 +0000

    changed readme and added coding notes

--- models/Table.class.php
+++ models/Table.class.php
@@ -5,8 +5,10 @@
  * This class should be a cross application parent class for providing a useful PDO tool for interacting with databases.
  *
  *	Classes extending this class should be made for application specific PDO transactions with databases.
+ *
+ * One extension of this class should be made for each (major?) table in the database.
  * 
- * The class requires a PDO object as an argument ($dbPlaceholder) when instantiating.
+ * The class requires a PDO object as an argument ($db) when instantiating.
  *
  * The constructor function assigns the PDO object to the protected property, Table->dbObj referred to as $this->dbObj in the definition.
  */
