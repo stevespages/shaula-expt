57578b75944b314bfd52e8a6a14c9f49993d4370
Steve Greig <greigSteve@gmail.com>
2017-11-30 12:05:13 +0000

    Added Smarty. Just sais 'Hello Ned' at the moment

--- front-controller.php
+++ front-controller.php
@@ -5,6 +5,17 @@
 
 //put error reporting statements here (will come after session_start() in admin.php)
 
+
+// set up Smarty templating system
+require('/usr/local/lib/php/Smarty/Smarty.class.php');
+$smarty = new Smarty();
+$smarty->setTemplateDir('/opt/lampp/htdocs/steve/shaula/smarty/templates');
+$smarty->setCompileDir('/opt/lampp/htdocs/steve/shaula/smarty/templates_c');
+$smarty->setCacheDir('/opt/lampp/htdocs/steve/shaula/smarty/cache');
+$smarty->setConfigDir('/opt/lampp/htdocs/steve/shaula/smarty/configs');
+$smarty->assign('name', 'Ned');
+$smarty->display('index.tpl');
+
 include_once "models/Page_Data.class.php";
 
 //this function returns the name of the controller file. Same name is used for navigation href values and in title elements.
---
+++ smarty/templates_c/fc56325121c97251e4c770c3b2b9a3c3c5b2cdfa_0.file.index.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-11-30 12:04:54
+  from "/opt/lampp/htdocs/steve/shaula/smarty/templates/index.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5a1fe5d68ff9f3_48461471',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'fc56325121c97251e4c770c3b2b9a3c3c5b2cdfa' => 
+    array (
+      0 => '/opt/lampp/htdocs/steve/shaula/smarty/templates/index.tpl',
+      1 => 1511999024,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a1fe5d68ff9f3_48461471 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<html>
+  <head>
+    <title>Smarty</title>
+  </head>
+  <body>
+    Hello, <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+!
+  </body>
+</html><?php }
+}
